468053cc549b2d63320d5f94ad4f77ef178f00df
Hoài Phương <internship04@softworld.jp>
2017-11-22 20:51:58 +0700

    master page

--- trunk/Web/Web.UI/Default.aspx.designer.cs
+++ trunk/Web/Web.UI/Default.aspx.designer.cs
@@ -11,23 +11,5 @@ namespace Web.UI {
     
     
     public partial class Default {
-        
-        /// <summary>
-        /// name control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label name;
-        
-        /// <summary>
-        /// btnLogout control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnLogout;
     }
 }
